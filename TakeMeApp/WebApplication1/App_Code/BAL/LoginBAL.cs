a6d3ea55901f8b42d6a46ad9778ccef0bbfb4abb
safwatgharra <safwatgharra@gmail.com>
2017-08-29 15:08:07 +0300

    try2

--- TakeMeApp/WebApplication1/App_Code/BAL/LoginBAL.cs
+++ TakeMeApp/WebApplication1/App_Code/BAL/LoginBAL.cs
@@ -11,7 +11,7 @@ public class LoginBAL
     LoginDBService loginDb = new LoginDBService();
     public LoginBAL()
     {
-        //try1
+        //try2
         // TODO: Add constructor logic here
         //
     }
